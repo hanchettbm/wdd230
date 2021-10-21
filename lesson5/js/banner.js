1a6506b09609f43cd9806ae78411ac0944d1f978
hanchettbm <hanchettbm@gmail.com>
2021-10-21 12:47:09 -0600

    Lower Image Size

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,6 +1,4 @@
 var day = new Date().toLocaleDateString('en-US', {weekday: 'long'});
-console.log(day);
 if (day == "Wednesday") {
     document.getElementById("pancakes").style.display = "block";
-    console.log("It Workded!")
 }
