9344d7d7c6d6cc4c0c18bd5082d4b635a894e5fe
hanchettbm <hanchettbm@gmail.com>
2021-11-10 17:24:16 -0700

    Week 6 fixes

--- lesson6/js/banner.js
+++ lesson6/js/banner.js
@@ -1,4 +1,4 @@
 var day = new Date().toLocaleDateString('en-US', {weekday: 'long'});
-if (day == "Wednesday") {
+if (day == "Friday") {
     document.getElementById("pancakes").style.display = "block";
 }
