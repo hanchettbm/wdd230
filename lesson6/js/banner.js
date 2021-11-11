b10ee02d1a6399719cca5013baa223db289dc7f8
hanchettbm <hanchettbm@gmail.com>
2021-11-10 17:27:56 -0700

    Week 6

--- lesson6/js/banner.js
+++
@@ -1,4 +0,0 @@
-var day = new Date().toLocaleDateString('en-US', {weekday: 'long'});
-if (day == "Friday") {
-    document.getElementById("pancakes").style.display = "block";
-}
--- lesson6/js/dates.js
+++
@@ -1,2 +0,0 @@
-var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-document.querySelector("#currentDate").textContent = new Date().toLocaleDateString('en-UK', options);
